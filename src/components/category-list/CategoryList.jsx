5417a5894da0852065c6775d9ab8e7c7225d8b85
Vishal <Vishalarya2468@gmail.com>
2024-04-12 15:33:03 +0530

    feat: featured post and category component design added

--- src/components/category-list/CategoryList.jsx
+++ src/components/category-list/CategoryList.jsx
@@ -1,9 +1,77 @@
-import styles from "./categorylist.module.css"
+import styles from "./categorylist.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const CategoryList = () => {
+  const CatListData = [
+    {
+      id: 1,
+      srcImg: "/style.png",
+      catName: "Style",
+      CatLink: "",
+      bgColor: "#57c4ff31",
+    },
+    {
+      id: 2,
+      srcImg: "/fashion.png",
+      catName: "Fashion",
+      CatLink: "",
+      bgColor: "#da85c731",
+    },
+    {
+      id: 3,
+      srcImg: "/food.png",
+      catName: "Food",
+      CatLink: "",
+      bgColor: "#7fb88133",
+    },
+    {
+      id: 4,
+      srcImg: "/travel.png",
+      catName: "Travel",
+      CatLink: "",
+      bgColor: "#ff795736",
+    },
+    {
+      id: 5,
+      srcImg: "/culture.png",
+      catName: "Culture",
+      CatLink: "",
+      bgColor: "#ffb04f45",
+    },
+    {
+      id: 6,
+      srcImg: "/coding.png",
+      catName: "Coding",
+      CatLink: "",
+      bgColor: "#5e4fff31",
+    },
+  ];
+
   return (
-    <div className={styles.container}>CategoryList</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
+        {CatListData.map((data) => (
+          <Link
+            href="/blog?cat=style"
+            className={styles.category}
+            style={{backgroundColor:`${data.bgColor}`}}
+            key={data.id}
+          >
+            <Image
+              src={data.srcImg}
+              alt=""
+              width={32}
+              height={32}
+              className={styles.image}
+            />
+            {data.catName}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default CategoryList;
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,9 +1,32 @@
-import styles from "./featured.module.css"
+import styles from "./featured.module.css";
+import Image from 'next/image'
 
 const Featured = () => {
   return (
-    <div className={styles.container}>Featured</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, blog readers here!</b> Discover new stories and creative ideas.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imageContainer}>
+          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <div className={styles.postTitle}>
+            Lorem ipsum dolor sit amet consectetur.
+          </div>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea
+            corrupti perferendis ut vitae quis est necessitatibus, odit
+            inventore. Consequuntur quam accusamus tenetur molestiae ea. Ducimus
+            eos ut pariatur fugit minima.
+          </p>
 
-export default Featured
+          <button className={styles.button}> Read More</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
